88c5fca5c3b16dc8f69fe9e71ece7c4eba7106fe
Durgesh Kudalkar <dkudalkarsr2j@gmail.com>
2022-04-21 23:09:18 +0530

    Home ready

---
+++ lib/globals.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Color? primary = Color(0xFF0CED7F);
+Color? secondary = Color(0xFF06371F);
+
+double getHeight(context) {
+  double height = MediaQuery.of(context).size.height;
+  return height;
+}
+
+double getWidth(context) {
+  double width = MediaQuery.of(context).size.width;
+  return width;
+}
